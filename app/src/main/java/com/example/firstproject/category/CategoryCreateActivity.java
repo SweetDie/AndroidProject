d08aee4a80fdfa1a90d5dc7b5b815c8a94a19cdd
Dima Potapchuk <traiget@gmail.com>
2023-04-24 20:14:00 +0300

    Create add category activity

---
+++ app/src/main/java/com/example/firstproject/category/CategoryCreateActivity.java
@@ -0,0 +1,18 @@
+package com.example.firstproject.category;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.firstproject.BaseActivity;
+import com.example.firstproject.R;
+
+public class CategoryCreateActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_create);
+    }
+}
